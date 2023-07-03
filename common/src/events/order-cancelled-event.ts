9f1d0b29f4c966e8387a3c45582dc2f5bbb40c5a
kobakamladze <kobakamladze08@gmail.com>
2023-07-03 12:28:17 +0400

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -1,7 +1,6 @@
 import { Subjects } from './subjects'
-import { OrderStatus } from './types/order-status'
 
-export interface OrderCCancelledEvent {
+export interface OrderCancelledEvent {
     subject: Subjects.OrderCancelled
     data: {
         id: string
